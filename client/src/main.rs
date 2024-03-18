3cec3ef8f6ba6a9b44f7de499e60e2d344a3fc1d
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-03-18 16:00:04 +0100

    yep... i broke it whoops

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,22 +1,36 @@
 use std::io::{Write, Result};
 use std::net::TcpStream;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
+use std::thread;
 
 fn main() -> Result<()>{
     match TcpStream::connect("127.0.0.1:8888") {
         Ok(mut stream) => {
             println!("Connected to 127.0.0.1:8888");
-            
+
+            let handle = stream.try_clone()?;
+            let reader = BufReader::new(handle);
+
+            // Spawn a thread to read from the stream and print received data
+            thread::spawn(move || {
+                for line in reader.lines() {
+                    println!("{}", line.unwrap());
+                }
+            });
+
+            // Read from stdin and send data over the stream
             loop {
                 let mut buffer = String::new();
-                let mut handle = io::stdin().lock();
-                handle.read_line(&mut buffer)?;
+                io::stdin().read_line(&mut buffer)?;
                 
-                stream.write(&mut buffer.as_bytes())?;
-            }
+                print!("\x1B[2K\r");
+
+                stream.write_all(buffer.as_bytes())?;
+            }        
         },
         Err(e) => {
             Err(e)
         },
     }
 }
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,87 @@
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, IpAddr};
 use std::io::{Result, Write, BufReader, BufRead};
 use std::thread;
 use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::str;
 
-struct Message;
-
-fn server_handler(_recevier: Receiver<Message>) -> Result<()> {
-    unimplemented!();
+// Clients are in groups and in a group, when a client sends a message all clients in the group
+// recive the message
+#[derive(Debug)]
+struct Group {
+    name: String,
+    clients: Vec<TcpStream>,
 }
 
-fn connection_handler(mut stream: TcpStream, _sender: Sender<Message>) -> Result<()> {
+impl Clone for Group {
+    fn clone(&self) -> Self {
+        let cloned_clients: Vec<TcpStream> = self.clients.iter()
+            .map(|stream| {
+                // Attempt to clone each TcpStream
+                stream.try_clone().expect("Failed to clone TcpStream")
+            })
+            .collect();
+        
+        Group {
+            name: self.name.clone(),
+            clients: cloned_clients,
+        }
+    }
+}
+
+impl Group {
+    fn new(name: String) -> Self {
+        Group {
+            name,
+            clients: vec![],
+        }
+    }
+
+    fn add_client(&mut self, mut client: TcpStream) {
+        self.clients.push(client);
+    }
+    
+    // sends a message to all clients in a group
+    fn broadcast(&self, message: Message) {
+        for mut stream in &self.clients {
+            writeln!(stream, "{:?}", message);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    message: String,
+    username: String,
+}
+
+fn server_handler(recevier: Receiver<Message>, groups: Arc<Mutex<Vec<Group>>>) -> Result<()> {
+    loop {
+        let groups = groups.lock().unwrap(); 
+        
+        for client in groups[0].clients.into_iter() {
+            writeln!(&client, "{:?}", recevier.recv());
+        }
+    }
+    Ok(())
+}
+
+fn connection_handler(mut stream: TcpStream, sender: Sender<Message>, groups: Arc<Mutex<Vec<Group>>>) -> Result<()> {
     writeln!(stream, "Welcome to the server!").map_err(|e|
         eprintln!("S_ ERROR: {}", e)
     ); 
 
+    let groups = groups.lock().unwrap();
+    groups[0].add_client(stream);
+
     loop {
         let mut reader = BufReader::new(&stream);
         for data in reader.lines() {
-            println!("{}", data.unwrap());
+            sender.send(Message {
+                message: data.unwrap().to_string(),
+                username: "User_1".to_string(),
+            });
+            // println!("{}", data.unwrap());
         } 
     }
 }
@@ -29,18 +92,26 @@ fn main() -> Result<()> {
 
     println!("S_ INFO: Bind listener to: {}", addr);
 
+    let mut groups = Arc::new(Mutex::new(vec![]));
+
+    let mut test_group = Group::new("test".to_string());
+
+    groups.lock().unwrap().push(test_group);
+
     let (sender, receiver) = channel();
     thread::spawn(move || {
-        server_handler(receiver);
+        server_handler(receiver, groups.clone());
     });
 
+    // handles incoming connection trys by unknown clients
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("S_ INFO: {} connected", stream.peer_addr()?);
                 let sender = sender.clone();
+                let groups = groups.clone();
                 thread::spawn(move || {
-                    connection_handler(stream, sender);
+                    connection_handler(stream, sender, groups);
                 });
             },
             Err(e) => {
